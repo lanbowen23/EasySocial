6b1290a5e5f06bd9a83b7b78def6cc274fffd9f2
lanbowen23 <lanbowen23@tamu.edu>
2019-10-15 20:45:52 -0500

    User Router Setup, check validate

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
-
 const app = express();
 
-app.get('/', (req, res) => res.send('API Running'));
+const connectDB = require('./config/db');
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+app.get('/', (req, res) => res.send('API Running'));
+
+// body parser
+// allow us to get data from req.body
+app.use(express.json({extended: false}));
+
+// Define Routes
+// don't forget the / in the beginning
+app.use('/api/users', require('./routers/api/users'));
